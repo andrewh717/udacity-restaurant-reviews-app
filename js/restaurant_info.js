0d48dacb798742a2dc598fa0a2f3d12c8941a296
Andrew H <andrewh717@gmail.com>
2020-01-09 21:29:08 -0500

    Initial commit

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYW5kcmV3aDcxNyIsImEiOiJjazU3amV6bzcwM3lsM2tvNm5qdjZtemdvIn0.NzrYDXrVrx7OqiHFAqkkQg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
